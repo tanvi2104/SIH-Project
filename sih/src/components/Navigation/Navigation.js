b770a3c4c5d5e80b493dc21b2148e031a922486e
Tanvi Singh <singhvi2104@gmail.com>
2023-09-14 23:02:03 +0530

    last try of api

--- sih/src/components/Navigation/Navigation.js
+++ sih/src/components/Navigation/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Navigation = () => {
+const Navigation = ({ onRouteChange }) => {
 	return (
 		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-			<p className='pt2 f4 link dim white pa3 pointer athelas'>About Us</p>
+			<p className='pt2 f4 link dim white pa3 pointer athelas'>
+			About Us</p>
 		</nav>
 	);
 }
