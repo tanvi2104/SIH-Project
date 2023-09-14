6be3eef3f95806b06d2e1ac4cb6a0302a1cbb1e6
Tanvi Singh <117470255+tanvi2104@users.noreply.github.com>
2023-09-14 23:52:18 +0530

    Update ImageLinkForm.js

--- sih/src/components/ImageLinkForm/ImageLinkForm.js
+++ sih/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputchange, onButtonSubmit }) => {
+const ImageLinkForm = () => {
 	return (
 		<div >
 			<p className='f3 white athelas pt3'>
@@ -9,9 +9,8 @@ const ImageLinkForm = ({ onInputchange, onButtonSubmit }) => {
 			</p>	
 			<div className='center'>
 				<div className='form center shadow-5 '>
-					<input className='f4 pa2 w-70 center' type='text' onChange={onInputchange} />
-					<button className='w-30 grow f4 link ph3 pv2 dib white bg-green' 
-					onClick={onButtonSubmit}>
+					<input className='f4 pa2 w-70 center' type='text' />
+					<button className='w-30 grow f4 link ph3 pv2 dib white bg-green' >
 					Analyze
 					</button>
 				</div>
