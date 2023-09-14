86fbae3bf581e32743ec5b3c524d1ce548e9d00b
Tanvi Singh <117470255+tanvi2104@users.noreply.github.com>
2023-09-14 23:52:39 +0530

    Update ImageLinkForm.js

--- sih/src/components/ImageLinkForm/ImageLinkForm.js
+++ sih/src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,7 @@ const ImageLinkForm = () => {
 			<div className='center'>
 				<div className='form center shadow-5 '>
 					<input className='f4 pa2 w-70 center' type='text' />
-					<button className='w-30 grow f4 link ph3 pv2 dib white bg-green' >
+					<button className='w-30 grow f4 link ph3 pv2 dib white bg-green'>
 					Analyze
 					</button>
 				</div>
