6f11e712ad41831d6870cf598aab31c93de79697
Tanvi Singh <singhvi2104@gmail.com>
2023-09-14 23:47:41 +0530

    changed back to previous version, deleted everything related to api

--- sih/src/components/Navigation/Navigation.js
+++ sih/src/components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Navigation = ({ onRouteChange }) => {
+const Navigation = () => {
 	return (
 		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 			<p className='pt2 f4 link dim white pa3 pointer athelas'>
