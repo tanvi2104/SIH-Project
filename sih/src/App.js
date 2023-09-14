04b68f94bc42bbb3199e8d91e275ce6093994ec4
Tanvi Singh <117470255+tanvi2104@users.noreply.github.com>
2023-09-14 23:50:42 +0530

    Update App.js

--- sih/src/App.js
+++ sih/src/App.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react';
 import Navigation from "./components/Navigation/Navigation";
 import Rank from "./components/Rank/Rank";
-import axios from 'axios';
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import './App.css';
-
-const app = new axios.App({ 
-  method: "POST",
-    url: "https://detect.roboflow.com/tree-top-view/2",
-    params: {
-        api_key: "rdlxQ2kUnRiOkQO6wjjD",
-        image: "IMAGE_URL"
-    }
-})
-.then(function(response) {
-    console.log(response.data);
-})
-.catch(function(error) {
-    console.log(error.message);
-});
-
-
 
 class App extends Component{
   constructor()
@@ -32,35 +14,13 @@ class App extends Component{
     }
   }
 
-  onInputChange =(event)=>{
-    this.setState({input: event.target.value});
-  }
-
-  onButtonSubmit = () => {
-    this.setState({image: this.state.input});
-    app.models.predict("rdlxQ2kUnRiOkQO6wjjD", this.state.input)
-    
-  }
-
-  onRouteChange= (route) =>{
-    if(route === 'back'){
-      this.setState({isAbout: false})
-    }
-    else if(route==='home'){
-      this.setState({isAbout: true})
-    }
-    this.setState({route:'home'});
-  }
   render(){
     return (
       <div className="App">
         <div>
           <Navigation />
           <Rank />
-          <ImageLinkForm 
-          onInputchange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit}
-          />
+          <ImageLinkForm />
         </div>
       </div>
     );
